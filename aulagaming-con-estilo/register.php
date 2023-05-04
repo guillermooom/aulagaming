f0df810d88c6f3ddbe55aa6cdfa495467e031d8a
guillermooom <97970403+guillermooom@users.noreply.github.com>
2023-05-04 08:14:19 +0200

    Update register.php

--- aulagaming-con-estilo/register.php
+++ aulagaming-con-estilo/register.php
@@ -2,11 +2,12 @@
 require "config/config.php";
 require "config/config-register.php";
 ?>
-<!DOCTYPE html>
 <html>
 <head>
 	<title>Registro</title>
+	<meta charset="UTF-8">
 	<a href="inicio.php">Inicio</a>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/base.css">
 </head>
 <body>
