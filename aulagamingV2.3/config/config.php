c6661d0a99c33c6e37560183469317649ed96f3e
AdrianAlonso2002 <114055056+AdrianAlonso2002@users.noreply.github.com>
2023-05-23 21:45:59 +0200

    Add files via upload

---
+++ aulagamingV2.3/config/config.php
@@ -0,0 +1,37 @@
+<?php
+function conexion()
+{
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "gaming";
+
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  return $conn;
+}
+
+//Limpiar variables.
+
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function inicioUsuario($conn, $nombre)
+{
+  $stmt = $conn->prepare("SELECT nombre,apellido FROM usuarios
+    WHERE email = :nombre ");
+
+  $stmt->bindParam(':nombre', $nombre);
+  $stmt->execute();
+
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $result = $stmt->fetchAll();
+
+  return $result;
+}
